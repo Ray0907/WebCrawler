9d3f0d692cee21003756325f1c0cd666ede6d833
Ray0907 <a0939552854@gmail.com>
2018-01-11 20:45:07 +0800

    no message

--- NBAScoreboard.py
+++ NBAScoreboard.py
@@ -3,12 +3,13 @@
 import requests
 import json
 print('NBA Scoreboard')
-date=input('Please input the date (ex0101):')
+print('Please input Tw date-1')
+date=input('Please input the date(ex0101):')
+print('-------------------------')
 url='https://data.nba.net/prod/v2/2018'+date+'/scoreboard.json'
 res=requests.get(url)
 html=res.text
 result=json.loads(html)
-
 for item in result['games']:
     hTeam=item.get('hTeam').get('triCode')
     hTeamScore=item.get('hTeam').get('score')
@@ -19,4 +20,6 @@ for item in result['games']:
     else:
         win=vTeam
     print(hTeam+': '+vTeam+' '+hTeamScore+':'+vTeamScore+' '+'%s win!!!' %win+'\n')
+    print('-------------------------')
+
 
